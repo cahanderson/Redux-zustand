f3bb979b217e71fbe0db671a1bacf5d945f667ca
Anderson <cahanderson@gmail.com>
2025-01-30 21:25:49 -0300

    commit parcial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { AddTodo } from "./components/AddTodo";
+import { TodoList } from "./components/TodoList";
+
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from "./store";
+
+export function App() {
+
+  return (
+    <ReduxProvider store={store}>
+      <TodoList />
+      <AddTodo />
+    </ReduxProvider>
+  )
+}
