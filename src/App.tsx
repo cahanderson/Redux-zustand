5994672f593a19d304f8b550ddbdafd1463a25df
Carlos Anderson <cahanderson@gmail.com>
2025-02-04 21:10:40 -0300

    commit parcial

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { AddTodo } from "./components/AddTodo";
-import { TodoList } from "./components/TodoList";
-
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from "./store";
+import { Player } from './pages/Player';
+import './styles/global.css';
 
 export function App() {
 
   return (
     <ReduxProvider store={store}>
-      <TodoList />
-      <AddTodo />
+      <Player />
     </ReduxProvider>
   )
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,13 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore} from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { player } from "./slices/player";
 
-const todoSlice = createSlice({
-    name:'todo',
-    initialState: ['Fazer cafe', 'Estudar Redux', 'Estudar Redux Toolkit'],
-
-    reducers: { 
-        add: (state, action) => {
-            state.push(action.payload.newTodo);
-        }
-    }
-});
 
 export const store = configureStore({
     reducer: {
-        todo: todoSlice.reducer,
+        player,
     },
 });
 
-export const { add } = todoSlice.actions;
+export type RootState = ReturnType<typeof store.getState>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
